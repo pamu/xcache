620ab0df203d3f81082695c0957a3d815207320c
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-08-26 15:34:57 +0530

    stats done

--- src/main/java/com/pamu_nagarjuna/xcache/Main.java
+++ src/main/java/com/pamu_nagarjuna/xcache/Main.java
@@ -1,5 +1,8 @@
 package com.pamu_nagarjuna.xcache;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -8,11 +11,50 @@ import java.util.concurrent.Executors;
  */
 public class Main {
     public static void main(String ... args) {
-        System.out.println("XCache");
-        long million = 1000000;
-        for(int tc = 1; tc < 100; tc++) {
+        System.out.println("XCache Performance analysis running ...");
+        final int million = 1000000;
+        XCache<Integer, Integer> xCache = new XCache<>(million);
+        for(int i = 0; i < million; i++) xCache.put(i, i);
+        File statsFile = new File(System.getProperty("user.home") + "/Desktop/stats.txt");
+        try {
+            PrintWriter writer = new PrintWriter(statsFile);
+            for(int tc = 1; tc <= 10; tc ++) {
+                double getTime = 0;
+                double putTime = 0;
+                for(int i = 0; i < 100; i ++) {
+                    long timeTakenPut = getRunningTime(tc, new Runnable() {
+                        @Override
+                        public void run() {
+                            putOperation(xCache, million);
+                        }
+                    });
+                    putTime = putTime + timeTakenPut;
+                }
 
+                putTime = putTime/100;
+
+                for(int i = 0;  i < 100; i ++) {
+                    long timeTakenGet = getRunningTime(tc, new Runnable() {
+                        @Override
+                        public void run() {
+                            getOperation(xCache, million);
+                        }
+                    });
+                    getTime = getTime + timeTakenGet;
+                }
+
+                getTime = getTime/100;
+
+                writer.println(tc + " " + (putTime) + " " + (getTime));
+                writer.flush();
+                xCache.getCache().clear();
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(0);
         }
+        System.out.println("Done ... ");
     }
 
     public static void putOperation(XCache<Integer, Integer> xCache, int operations) {
